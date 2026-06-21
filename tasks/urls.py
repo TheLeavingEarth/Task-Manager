dd4950da95151a5fb21ba782961dca811550f222
Arseniy <theleavingearth@gmail.com>
2026-06-21 16:03:05 +0300

    fix deploy

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.task_list, name='list'),
-	path('add/', views.add_task, name='add'),
-	path('delete/<int:id>/', views.delete_task, name='delete'),
-	path('status/<int:id>/<str:status>/', views.change_status, name='change_status'),
-	path('edit/<int:id>/', views.edit_task, name='edit'),
+    path('', views.task_list, name='home'),
 ]
